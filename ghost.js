094324e14b552dd0a137d9588efa0c9071ab7b48
טל מלול <talmalu@auth.ad.bgu.ac.il>
2019-04-21 19:17:11 +0300

    spooky

---
+++ ghost.js
@@ -0,0 +1,26 @@
+
+
+
+    var position = new Object();
+    position.x = 300;
+    position.y = 300;
+    var color = "black";
+
+
+    function draw_ghost(ctx) {
+
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.moveTo(83, 116);
+        ctx.lineTo(83, 102);
+        ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+        ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+        ctx.lineTo(111, 116);
+        ctx.lineTo(106.333, 111.333);
+        ctx.lineTo(101.666, 116);
+        ctx.lineTo(97, 111.333);
+        ctx.lineTo(92.333, 116);
+        ctx.lineTo(87.666, 111.333);
+        ctx.lineTo(83, 116);
+        ctx.fill();
+    }
