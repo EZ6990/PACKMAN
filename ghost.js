9a67d2be84204e3d3465869b935ea4ffb1d9563d
דניאל קויפמן <koyfdan@auth.ad.bgu.ac.il>
2019-04-21 21:00:13 +0300

    the greates monster u ever saw

--- ghost.js
+++ ghost.js
@@ -1,26 +1,77 @@
 
 
 
-    var position = new Object();
-    position.x = 300;
-    position.y = 300;
-    var color = "black";
+var position = new Object();
+position.x = 300;
+position.y = 300;
+var ghostColor = "red";
+var mult=2;
 
+function draw_ghost(ctx, centerX, centerY) {
+    centerX-=30;
+    centerY+=30;
+    ctx.beginPath();
+    ctx.fillStyle = ghostColor;
+    //ctx.moveTo(83, 116);
+    ctx.moveTo(centerX, centerY);
+    //ctx.lineTo(83, 102);
+    ctx.lineTo(centerX, centerY - 14 *mult);
+    //ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+    ctx.bezierCurveTo(centerX, centerY - 22*mult, centerX + 6*mult, centerY - 28*mult, centerX + 14*mult, centerY - 28*mult);
+    // ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+    ctx.bezierCurveTo(centerX + 22*mult, centerY - 28*mult, centerX + 28*mult, centerY - 22*mult, centerX + 28*mult, centerY - 14*mult);
+    //ctx.lineTo(111, 116);
+    ctx.lineTo(centerX+28*mult, centerY);
+    //ctx.lineTo(106.333, 111.333);
+    ctx.lineTo(centerX+23.333*mult, centerY+(-5+0.333)*mult);
+    //ctx.lineTo(101.666, 116);
+    ctx.lineTo(centerX+18.666*mult,centerY);
+    //ctx.lineTo(97, 111.333);
+    ctx.lineTo(centerX+14*mult, centerY+(-5+0.333)*mult);
+    //ctx.lineTo(92.333, 116);
+    ctx.lineTo(centerX+9.333*mult, centerY);
+    //ctx.lineTo(87.666, 111.333);
+    ctx.lineTo(centerX+4.666*mult,centerY+(-5+0.333)*mult);
+    //ctx.lineTo(83, 116);
+    ctx.lineTo(centerX, centerY);
+    ctx.closePath();
+    ctx.fill();
 
-    function draw_ghost(ctx) {
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    //ctx.moveTo(91, 96);
+    ctx.moveTo(centerX+8*mult, centerY-20*mult);
+    //ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+    ctx.bezierCurveTo(centerX+5*mult, centerY-20*mult, centerX+4*mult, centerY-17*mult, centerX+4*mult, centerY-15*mult);
+    //ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+    ctx.bezierCurveTo(centerX+4*mult, centerY-13*mult, centerX+5*mult, centerY-10*mult, centerX+8*mult, centerY-10*mult);
+    //ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+    ctx.bezierCurveTo(centerX+11*mult, centerY-10*mult, centerX+12*mult, centerY-13*mult, centerX+12*mult, centerY-15*mult);
+    //ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+    ctx.bezierCurveTo(centerX+12*mult, centerY-17*mult, centerX+11*mult, centerY-20*mult, centerX+8*mult, centerY-20*mult);
+    //ctx.moveTo(103, 96);
+    ctx.moveTo(centerX+20*mult,centerY-20*mult);
+    //ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+    ctx.bezierCurveTo(centerX+17*mult, centerY-20*mult, centerX+16*mult, centerY-17*mult, centerX+16*mult, centerY-15*mult);
+    //ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+    ctx.bezierCurveTo(centerX+16*mult, centerY-13*mult, centerX+17*mult, centerY-10*mult, centerX+20*mult, centerY-10*mult);
+   // ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+    ctx.bezierCurveTo(centerX+23*mult, centerY-10*mult, centerX+24*mult, centerY-13*mult, centerX+24*mult, centerY-15*mult);
+    //ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+    ctx.bezierCurveTo(centerX+24*mult, centerY-17*mult, centerX+23*mult, centerY-20*mult, centerX+20*mult, centerY-20*mult);
+    ctx.closePath();
+    ctx.fill();
 
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.moveTo(83, 116);
-        ctx.lineTo(83, 102);
-        ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
-        ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
-        ctx.lineTo(111, 116);
-        ctx.lineTo(106.333, 111.333);
-        ctx.lineTo(101.666, 116);
-        ctx.lineTo(97, 111.333);
-        ctx.lineTo(92.333, 116);
-        ctx.lineTo(87.666, 111.333);
-        ctx.lineTo(83, 116);
-        ctx.fill();
-    }
+    ctx.fillStyle = ghostColor;
+    ctx.beginPath();
+    //ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+    ctx.arc(centerX+18*mult,centerY-14*mult, 2, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.beginPath();
+    //ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+    ctx.arc(centerX+6*mult, centerY-14*mult, 2, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+}
--- pacman.js
+++ pacman.js
@@ -1,8 +1,8 @@
 /*javascript */
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-var context = canvas.getContext("2d");
+    var context = canvas.getContext("2d");
     var shape = new Object();
     var board;
     var score;
@@ -10,37 +10,40 @@ var context = canvas.getContext("2d");
     var start_time;
     var time_elapsed;
     var interval;
-    var attempts=3;
-    var counterR=0;
+    var attempts = 3;
+    var counterR = 0;
     var then;
     var timeLeft = 60000;
-    var r=0;
+    var r = 0;
+
     Start();
 
-    function timeCountDown(){
+    function timeCountDown() {
         var now = Date.now();
-	    var delta = now - then;
-	    timeLeft-=delta;
-	    if(timeLeft<=0)
+        var delta = now - then;
+        timeLeft -= delta;
+        if (timeLeft <= 0)
             timeLeft = 0;
-            lblTime.value=timeLeft/1000;
-        then=now;
+        lblTime.value = timeLeft / 1000;
+        then = now;
     }
     function Start() {
         board = new Array();
         score = 0;
-        lbAttempts.innerText=attempts;
+        lbAttempts.innerText = attempts;
         pac_color = "yellow";
         var cnt = 100;
         var food_remain = 50;
         var pacman_remain = 1;
         start_time = new Date();
-                
+
         for (var i = 0; i < 10; i++) {
             board[i] = new Array();
             //put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
             for (var j = 0; j < 10; j++) {
-                if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
+                if (i === 0 && j === 0)
+                    board[i][j] = 3;
+                else if ((i === 3 && j === 3) || (i === 3 && j === 4) || (i === 3 && j === 5) || (i === 6 && j === 1) || (i === 6 && j === 2)) {
                     board[i][j] = 4;
                 } else {
                     var randomNum = Math.random();
@@ -50,7 +53,7 @@ var context = canvas.getContext("2d");
                     } else if (randomNum < 1.0 * (pacman_remain + food_remain) / cnt) {
                         shape.i = i;
                         shape.j = j;
-                        shape.direction=(Math.random()*4)|0+1;
+                        shape.direction = (Math.random() * 4) | 0 + 1;
                         pacman_remain--;
                         board[i][j] = 2;
                     } else {
@@ -73,9 +76,9 @@ var context = canvas.getContext("2d");
             keysDown[e.code] = false;
         }, false);
         interval = setInterval(UpdatePosition, 100);
-        mouthInterval=setInterval(function(){r=Math.abs(Math.sin(++counterR))*15/100;},100);
-        then=Date.now();
-        setInterval(timeCountDown,1);
+        mouthInterval = setInterval(function () { r = Math.abs(Math.sin(++counterR)) * 15 / 100; }, 100);
+        then = Date.now();
+        setInterval(timeCountDown, 1);
     }
 
 
@@ -110,8 +113,8 @@ var context = canvas.getContext("2d");
     function Draw() {
         context.clearRect(0, 0, canvas.width, canvas.height); //clean board
         lblScore.value = score;
-       // lblTime.value = time_elapsed;
-        var specialCandy=Math.random();
+        // lblTime.value = time_elapsed;
+        var specialCandy = Math.random();
 
         for (var i = 0; i < 10; i++) {
             for (var j = 0; j < 10; j++) {
@@ -120,21 +123,24 @@ var context = canvas.getContext("2d");
                 center.y = j * 60 + 30;
                 if (board[i][j] === 2) {
                     context.beginPath();
-                    context.arc(center.x, center.y, 30, (0.15-r) * Math.PI+(shape.direction-1)*Math.PI/2, (1.85 +r)* Math.PI+(shape.direction-1)*Math.PI/2); // half circle
+                    context.arc(center.x, center.y, 30, (0.15 - r) * Math.PI + (shape.direction - 1) * Math.PI / 2, (1.85 + r) * Math.PI + (shape.direction - 1) * Math.PI / 2); // half circle
                     context.lineTo(center.x, center.y);
                     context.fillStyle = pac_color; //color
                     context.fill();
                     context.beginPath();
-                    context.arc(center.x + ((1-shape.direction)*(shape.direction%2)+1)*(15-(shape.direction%2)*10) , center.y - ((shape.direction-4)*((shape.direction+1)%2)+1)*(15-((shape.direction+1)%2)*10) , 5,0, 2 * Math.PI); // circle
+                    context.arc(center.x + ((1 - shape.direction) * (shape.direction % 2) + 1) * (15 - (shape.direction % 2) * 10), center.y - ((shape.direction - 4) * ((shape.direction + 1) % 2) + 1) * (15 - ((shape.direction + 1) % 2) * 10), 5, 0, 2 * Math.PI); // circle
                     context.fillStyle = "black"; //color
                     context.fill();
                 } else if (board[i][j] === 1) {
-                   
+
                     context.beginPath();
                     context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
                     context.fillStyle = "black"; //color
                     context.fill();
-                } else if (board[i][j] === 4) {
+                } else if (board[i][j] === 3) {
+                    draw_ghost(context,center.x,center.y);
+                }
+                else if (board[i][j] === 4) {
                     context.beginPath();
                     context.rect(center.x - 30, center.y - 30, 60, 60);
                     context.fillStyle = "grey"; //color
@@ -146,26 +152,25 @@ var context = canvas.getContext("2d");
                     context.fillStyle = "green"; //color
                     context.fill();
                 }
-                
+
             }
         }
-        var RandX=Math.floor((Math.random() * 9) + 1);
-        var RandY=Math.floor((Math.random() * 9) + 1);
-        if(board[RandX][RandY] == 0){
-            var specialCandy=Math.random();
-            if(specialCandy<0.05)
-            {
+        var RandX = Math.floor((Math.random() * 9) + 1);
+        var RandY = Math.floor((Math.random() * 9) + 1);
+        if (board[RandX][RandY] == 0) {
+            var specialCandy = Math.random();
+            if (specialCandy < 0.05) {
                 center.x = RandX * 60 + 30;
                 center.y = RandY * 60 + 30;
                 context.beginPath();
                 context.arc(center.x, center.y, 25, 0, 2 * Math.PI); // circle
                 context.fillStyle = "green"; //color
                 context.fill();
-                board[RandX][RandY]=5;
+                board[RandX][RandY] = 5;
             }
         }
     }
-    
+
 
     function UpdatePosition() {
         board[shape.i][shape.j] = 0;
@@ -174,33 +179,33 @@ var context = canvas.getContext("2d");
         if (x === 1) {
             if (shape.j > 0 && board[shape.i][shape.j - 1] !== 4) {
                 shape.j--;
-                shape.direction=4;            
+                shape.direction = 4;
             }
         }
         if (x === 2) {
             if (shape.j < 9 && board[shape.i][shape.j + 1] !== 4) {
                 shape.j++;
-                shape.direction=2;
+                shape.direction = 2;
             }
         }
         if (x === 3) {
             if (shape.i > 0 && board[shape.i - 1][shape.j] !== 4) {
                 shape.i--;
-                shape.direction=3;
+                shape.direction = 3;
             }
 
         }
         if (x === 4) {
             if (shape.i < 9 && board[shape.i + 1][shape.j] !== 4) {
                 shape.i++;
-                shape.direction=1;
+                shape.direction = 1;
             }
         }
         if (board[shape.i][shape.j] === 1) {
             score++;
         }
         if (board[shape.i][shape.j] === 5) {
-            score+=5;
+            score += 5;
         }
         board[shape.i][shape.j] = 2;
         var currentTime = new Date();
